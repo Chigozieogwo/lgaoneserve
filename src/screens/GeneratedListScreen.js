fd17390687845f0074199ae0183ec87ce3221953
chigozie <ogwoekechimic@gmail.com>
2023-01-14 11:40:37 +0100

    download reverse

--- src/screens/GeneratedListScreen.js
+++ src/screens/GeneratedListScreen.js
@@ -137,7 +137,7 @@ const BatchPreviewScreen = ({ match }) => {
             </tr>
         </thead>
         <tbody>
-                                          {demand_lists.map((demand_list, index) => (
+                                          {demand_lists.reverse().map((demand_list, index) => (
                                             <tr key={demand_list._id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                             <td class="w-4 p-4">
                                                 <div class="flex items-center">
