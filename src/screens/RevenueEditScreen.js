13ea1288bf232645cf1bb7f83cd91ba08b1cd482
chigozie <ogwoekechimic@gmail.com>
2023-02-08 23:35:02 +0100

    theme save

--- src/screens/RevenueEditScreen.js
+++ src/screens/RevenueEditScreen.js
@@ -323,7 +323,7 @@ id="lga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-
                                        <button
                                           onClick={submitHandler}
                                           type="submit"
-                                          class="w-full text-white bg-blue-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-600"
+                                          class="w-full text-white bg-green-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-600"
                                        >
                                           Edit Revenue
                                        </button>
