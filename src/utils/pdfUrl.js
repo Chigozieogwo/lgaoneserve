cb582ee97e9a8740fae1ad168361b7a9220627f6
chigozie <ogwoekechimic@gmail.com>
2023-03-02 15:23:07 +0100

    repom

--- src/utils/pdfUrl.js
+++ src/utils/pdfUrl.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 
-let pdfUrl = 'https://app-api.billable.site'
+let pdfUrl1 = "https://app-api.billable.site"
+let pdfUrl = "https://api.billable.site"
 
 export default pdfUrl
